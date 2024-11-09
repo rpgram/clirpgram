177016b81b28512883f5c387ffc94621f99525d2
Juxan <julyfortune101@gmail.com>
2024-11-09 19:58:06 +0300

    application config with urls done

---
+++ src/application/configuration.rs
@@ -0,0 +1,31 @@
+use std::fs::File;
+use std::io::Read;
+use toml::Table;
+
+struct Config {
+    pub world_url: String,
+    pub battlefield_url: String,
+}
+
+fn read_config(file_path: String) -> Config {
+    let mut opened = File::open(file_path.clone()).expect(format!("{} doesnt exists", file_path).as_str());
+    let mut config_buffer = [0u8; 4096];
+    let got = opened.read(&mut config_buffer).unwrap();
+    let toml = String::from_utf8(config_buffer[..got].to_vec()).unwrap().as_str().parse::<Table>().expect("Invalid config format.");
+    Config {
+        world_url: toml["world_url"].as_str().unwrap().to_string(),
+        battlefield_url: toml["battlefield_url"].as_str().unwrap().to_string()
+    }
+}
+
+#[cfg(test)]
+mod configurator {
+    use crate::application::configuration::read_config;
+
+    #[test]
+    fn test_config() {
+        let config = read_config("config_sample.toml".to_string());
+        assert_eq!(config.world_url, "http://localhost:8002".to_string());
+        assert_eq!(config.battlefield_url, "http://localhost:8000".to_string())
+    }
+}
--- src/application/mod.rs
+++ src/application/mod.rs
@@ -1,1 +1,2 @@
 pub mod start_battle;
+mod configuration;
